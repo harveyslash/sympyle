769b04548b2819c9a790cab9de95f5d2d2a6443a
harshvardhangupta <harshvardhan_gupta@yahoo.com>
2018-07-10 21:34:02 +0530

    Added mse test case

--- tests/Node/Ops/test_mse.py
+++ tests/Node/Ops/test_mse.py
@@ -13,22 +13,62 @@ class MseOpTest(unittest.TestCase):
         This test passes if the two values are within some error tolerance
         :return:
         """
+
+        # Setup graph with random values
         np_inputs = np.random.randn(1)
         np_targets = np.random.randn(1)
         eps = 0.000001
-        error_tolerance = 0.0000000001
+        error_tolerance = 0.0000001
 
-        inputs_min_eps = Tensor(np_inputs - eps)
-        targets_min_eps = Tensor(np_targets - eps)
-        mse_min_eps = MSE(inputs_min_eps, targets_min_eps)
-        y_min_eps = mse_min_eps.forward()
+        inputs = Tensor(np_inputs)
+        targets = Tensor(np_targets)
+        mse = MSE(inputs, targets)
+        y_min_eps = mse.forward()
+        mse.backward()
 
-        inputs_plus_eps = Tensor(np_inputs + eps)
-        targets_plus_eps = Tensor(np_targets + eps)
-        mse_plus_eps = MSE(inputs_plus_eps, targets_plus_eps)
-        y_plus_eps = mse_plus_eps.forward()
+        # compute derivatives from graph
+        inputs_derivative = inputs.backward_val.copy()
+        targets_derivative = targets.backward_val.copy()
 
-        assert (np.abs(y_min_eps - y_plus_eps) < error_tolerance)
+        mse.clear()
+
+        # modify inputs to inputs - eps
+        inputs.value = np_inputs - eps
+        targets.value = np_targets
+
+        y_min_eps = mse.forward()
+
+        mse.clear()
+
+        # modify inputs to inputs + eps
+        inputs.value = np_inputs + eps
+        targets.value = np_targets
+        y_plus_eps = mse.forward()
+
+        mse.clear()
+
+        numerical_deriv = (y_plus_eps - y_min_eps) / (2 * eps)
+
+        assert (np.abs(numerical_deriv - inputs_derivative) < error_tolerance)
+
+        # Compare derivatives of targets
+
+        inputs.value = np_inputs
+        # modify targets to targets - eps
+
+        targets.value = np_targets - eps
+        y_min_eps = mse.forward()
+
+        mse.clear()
+
+        inputs.value = np_inputs
+        # modify targets to targets - eps
+        targets.value = np_targets + eps
+        y_plus_eps = mse.forward()
+
+        numerical_deriv = (y_plus_eps - y_min_eps) / (2 * eps)
+
+        assert (np.abs(numerical_deriv - targets_derivative) < error_tolerance)
 
 
 if __name__ == "__main__":
