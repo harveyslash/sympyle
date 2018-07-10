09f302532ffe4d53ecfa85442ad2aa2da7237882
harshvardhangupta <harshvardhan_gupta@yahoo.com>
2018-07-10 19:40:42 +0530

    Added travis.yml

--- me.py
+++ me.py
@@ -1,38 +1,48 @@
-from Nodes import Tensor, Matmul, MSE, Relu, Add
+from Nodes import Tensor, Matmul, MSE, Relu, Add, SoftmaxWithCrossEntropy
+from Nodes import Node
 import numpy as np
 
 np.set_printoptions(suppress=True)
 np.random.seed(100)
 
-X = np.linspace(-np.pi, np.pi, 1).reshape(-1, 1).astype(np.float32)
-X_copy = X.copy()
-pure = np.cos(X)
+X = np.linspace(10, 10, 100).reshape(-1, 2).astype(np.float32)
 
-Y = pure + np.random.randn(*X.shape) * .07
-Y = Y.astype(np.float32)
+Y_temp = np.random.randint(0, 2, size=X.shape[:1])
+Y = np.zeros(shape=(50, 2))
 
-I = X.reshape(-1, 1)  # an input with minibatch size 100 and 1 feature
+Y[np.arange(50), Y_temp] = 1
 
-L1 = np.random.randn(1, 1).astype(np.float32)
-L1_b = np.random.randn(1, 1).astype(np.float32)
-L1_b2 = np.random.randn(1, 1).astype(np.float32)
+X_t = Tensor(X)
+Y_t = Tensor(Y)
 
-T = Y.reshape(-1, 1)
+output = SoftmaxWithCrossEntropy(X_t, Y_t)
+# print(I_t.value)
+# print(I_t2.value)
 
-I_t = Tensor(I)
-I_t2 = Tensor(I)
-T_t = Tensor(T)
-T_t2 = Tensor(T)
-
-head1 = Tensor(I) + Tensor(I)
-
-branch1 = head1 @ Tensor(I)
-branch2 = head1 @ Tensor(I)
-loss1 = MSE(branch1, T_t)
-loss2 = MSE(branch2, T_t2)
-final = loss1 + loss2 + loss2 + loss1
+# branch1 = head1 @ Tensor(I)
+# branch2 = head1 @ Tensor(I)
+# loss1 = MSE(branch1, T_t)
+# loss2 = MSE(branch2, T_t2)
+# final = Tensor(I) + loss2  # + loss2 + loss1 + loss1 + loss1 + loss1
+# final = Add(Tensor(I), loss2)
 # final = loss1 + loss2 + loss2
 
-# output.backward()
+
+for i in range(10):
+    output_val = output.forward()
+    print(output_val)
+
+    output.backward()
+    # print(X_t.backward_val)
+    grad = X_t.backward_val
+    # print(grad)
+
+    X_t.value -= 1 * grad
+    output.clear()
+# print(X_t.value)
+# print(X_t.backward_val * X_t.value)
+# output.backward(parent_grads=np.array([1.0]))
 # print(I_t.backward_val)
-final.draw_graph("gaha.png")
+output.draw_graph("gaha.png")
+
+# print(Node.consts.no_grads)
