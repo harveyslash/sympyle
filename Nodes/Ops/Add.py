Fixed add op with correct broadcasting semantics. Updated numerical gradient calculation to perform sum.

Nodes/Ops/Add.py
@@ -1,6 +1,7 @@
 from ..Node import Node
 from Nodes.Node import consts
 import numpy as np
+from Nodes import Utils
 
 
 class Add(Node):
@@ -21,21 +22,16 @@ class Add(Node):
 
         back = parent_grads
         if back is None:
-            back = np.array([1.0])
-            return back
+            back = np.ones_like(self.forward())
 
         if respect_to_node == self.children[0]:
             child = self.children[0].forward()
         elif respect_to_node == self.children[1]:
             child = self.children[1].forward()
+        else:
+            return None
 
-        dim1 = dim2 = 0
-        for i, (dim1, dim2) in enumerate(
-                zip(child.shape[::-1], back.shape[::-1])):
-            if dim2 != dim1:
-                break
+        broadcast_dims = Utils.get_broadcast_axes(back, child)
 
-        if dim1 == dim2:
-            return back
-
-        return back.sum(axis=tuple(range(i)))
+        grad = back.sum(axis=broadcast_dims).reshape(child.shape)
+        return grad

tests/Node/Ops/Helpers.py
@@ -6,7 +6,7 @@ __author__ = "Harshvardhan Gupta"
 import numpy as np
 
 
-def calculate_numerical_gradient(node, respect_to_node, node_slice=None,
+def calculate_numerical_gradient(node, respect_to_node, node_slice,
                                  eps=0.000001):
     """
     Calculate gradient numerically using the formula:
@@ -32,7 +32,7 @@ def calculate_numerical_gradient(node, respect_to_node, node_slice=None,
     """
     sliced = respect_to_node.value[node_slice]
 
-    if len(sliced) > 1:
+    if not np.isscalar(sliced):
         raise AttributeError("Slice should result in a single value")
 
     node.clear()
@@ -46,4 +46,5 @@ def calculate_numerical_gradient(node, respect_to_node, node_slice=None,
 
     respect_to_node.value[node_slice] += eps
 
-    return (plus_eps - minus_eps) / (2 * eps)
+    grad = (plus_eps - minus_eps) / (2 * eps)
+    return grad.sum()

tests/Node/Ops/test_add.py
@@ -11,12 +11,6 @@ class Add_Op(unittest.TestCase):
     Class for testing functionality of Addition operation
     """
 
-    def setUp(self):
-        pass
-
-    def tearDown(self):
-        pass
-
     def test_scalar_forward(self):
         """
         Test to see if scalar forward values are computed correctly
@@ -31,12 +25,7 @@ class Add_Op(unittest.TestCase):
 
         add_op.forward()
 
-        e = f'''
-        Test Failed!: test_scalar_forward()
-        add_op.forward_val = {add_op.forward_val}, 
-        (a+b) = {(a+b)}'''
-
-        assert add_op.forward_val == (a + b), e
+        assert add_op.forward_val == (a + b)
 
     def test_vector_forward(self):
         """
@@ -52,12 +41,7 @@ class Add_Op(unittest.TestCase):
 
         add_op.forward()
 
-        e = f'''
-        Test Failed!: test_vector_forward()
-        add_op.forward_val = {add_op.forward_val}, 
-        (a+b) = {(a+b)}'''
-
-        assert np.all(add_op.forward_val == (a + b)), e
+        assert np.all(add_op.forward_val == (a + b))
 
     def test_scalar_backward(self):
         """
@@ -75,13 +59,43 @@ class Add_Op(unittest.TestCase):
         t2_grad = b.backward_val
 
         a_numeric_grad = calculate_numerical_gradient(add_op, a,
-                                                      slice(0, None))
+                                                      (0,))
         b_numeric_grad = calculate_numerical_gradient(add_op, b,
-                                                      slice(0, None))
+                                                      (0,))
 
         assert np.abs(t1_grad - a_numeric_grad) < 0.00001
         assert np.abs(t2_grad - b_numeric_grad) < 0.00001
 
+    def test_broadcast_backward(self):
+        """
+        Test broadcast functionality.
+        Arrays of different shapes are broadcast together.
+        The values of the gradients are compared with the gradients
+        from the slope formula.
+        """
 
-if __name__ == "__main__":
-    unittest.main()
+        np.random.seed(100)
+
+        a = Tensor(np.random.randn(1, 1, 10, 3))
+        b = Tensor(np.random.randn(1, 10, 3, 10, 1))
+
+        a_idx = (0, 0, 1, 1)
+        b_idx = (0, 1, 1, 1, 0)
+
+        add_op = a + b
+
+        forward_val = add_op.forward()
+        assert forward_val.shape == (1, 10, 3, 10, 3)
+        add_op.backward()
+
+        a_grad = a.backward_val
+        b_grad = b.backward_val
+
+        assert a.value.shape == a.backward_val.shape
+        assert b.value.shape == b.backward_val.shape
+
+        a_numeric_grad = calculate_numerical_gradient(add_op, a, a_idx)
+        b_numeric_grad = calculate_numerical_gradient(add_op, b, b_idx)
+
+        assert np.abs(a_grad[a_idx] - a_numeric_grad) < 0.000001
+        assert np.abs(b_grad[b_idx] - b_numeric_grad) < 0.000001

tests/Node/Ops/test_mse.py
@@ -32,14 +32,12 @@ class MseOpTest(unittest.TestCase):
         targets_derivative = targets.backward_val.copy()
 
         mse.clear()
-        inputs_numerical_deriv = calculate_numerical_gradient(mse, inputs)
-        targets_numerical_deriv = calculate_numerical_gradient(mse, targets)
+        inputs_numerical_deriv = calculate_numerical_gradient(mse, inputs,
+                                                              (0,))
+        targets_numerical_deriv = calculate_numerical_gradient(mse, targets,
+                                                               (0,))
 
         assert (np.abs(
                 inputs_numerical_deriv - inputs_derivative) < error_tolerance)
         assert (np.abs(
                 targets_numerical_deriv - targets_derivative) < error_tolerance)
-
-
-if __name__ == "__main__":
-    unittest.main()

tests/Node/Ops/test_softmax_with_crossentropy.py
@@ -21,7 +21,7 @@ class SoftmaxWithCrossEntropyOpTest(unittest.TestCase):
         np_inputs = np.random.randn(5)
         np_targets = np.zeros(5)
         np_targets[0] = 1.0
-        array_slice = slice(0, 1)
+        array_slice = (0,)
 
         error_tolerance = 0.00000001
 
@@ -39,7 +39,3 @@ class SoftmaxWithCrossEntropyOpTest(unittest.TestCase):
 
         assert np.all(np.abs(
                 inputs_numerical_deriv - inputs_derivative) < error_tolerance)
-
-
-if __name__ == "__main__":
-    unittest.main()