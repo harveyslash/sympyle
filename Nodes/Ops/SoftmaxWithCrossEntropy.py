Fixed softmax with cross entropy. Added test for softmax with cross entropy. Fixed numerical derivative helper

Nodes/Ops/SoftmaxWithCrossEntropy.py
@@ -1,6 +1,7 @@
 from ..Node import Node
 from Nodes.Node import consts
 import numpy as np
+from functools import reduce
 
 
 class SoftmaxWithCrossEntropy(Node):
@@ -10,6 +11,7 @@ class SoftmaxWithCrossEntropy(Node):
     def __init__(self, output, targets, axis=1):
         super().__init__([output, targets])
         self.axis = axis
+        self.batch_axis = tuple(range(0, axis))
         self.softmax = None
 
     def forward(self):
@@ -23,15 +25,21 @@ class SoftmaxWithCrossEntropy(Node):
 
         cross_entropies = - targets * np.log(softmax)
 
-        cross_entropies = np.sum(cross_entropies, axis=1)
+        cross_entropies = np.sum(cross_entropies, axis=self.axis)
         self.softmax = softmax
 
-        return np.average(cross_entropies)
+        if len(self.batch_axis) > 0:
+            return np.average(cross_entropies, axis=self.batch_axis)
+
+        return cross_entropies
 
     def backward(self, respect_to_node, parent_grads=1.0, **kwargs):
+        self.forward()
         if respect_to_node == self.children[0]:
             softmax_output = self.softmax
             targets = self.children[1].forward()
-            return (softmax_output - targets) / softmax_output.shape[0]
+            return (softmax_output - targets) / reduce(lambda x, y: x * y,
+                                                       self.batch_axis + (1,))
+
         elif respect_to_node == self.children[1]:
             return consts.no_grads

tests/Node/Ops/Helpers.py
@@ -29,9 +29,9 @@ def calculate_numerical_gradient(node, respect_to_node, node_slice=None,
     :return:        the numerically computed gradient value
     """
     sliced = respect_to_node.value[node_slice]
-    if not isinstance(sliced, np.ndarray) or len(sliced) < 1:
-        raise AttributeError("Slice results in a scalar or Empty value"
-                             ".An array must be returned after slicing")
+
+    if len(sliced) > 1:
+        raise AttributeError("Slice should result in a single value")
 
     node.clear()
     respect_to_node.value[node_slice] += eps